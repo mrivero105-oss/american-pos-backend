f0a0021662a686dccbbcfb6fd2059bbbc83c0ab1
mrivero105-oss <mrivero105@gmail.com>
2025-11-25 07:10:51 -0400

    Fix: Add config.js to backend public folder for Render deployment

---
+++ public/js/config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+// IMPORTANT: This file contains sensitive keys and should NOT be committed to version control.
+export const firebaseConfig = {
+    apiKey: "AIzaSyByNb2B41XzZEOnuB63z0z4VIwp_patqaA",
+    authDomain: "american-pos.firebaseapp.com",
+    projectId: "american-pos",
+    storageBucket: "american-pos.firebasestorage.app",
+    messagingSenderId: "248248095933",
+    appId: "1:248248095933:web:b0f75f9e5dfbc4594e93d4"
+};
